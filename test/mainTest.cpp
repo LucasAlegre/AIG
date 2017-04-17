c5a2210863528be1f5a1fa5351b7dc1d4e163913
Marcelo Bittencourt <tchelopoa@gmail.com>
2017-04-16 21:10:38 -0300

    Inicial setup of test C17 using CUTE

--- test/mainTest.cpp
+++ test/mainTest.cpp
@@ -10,56 +10,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../src/PointerBased Graph/Graph/AIGNode.h"
-#include "../src/PointerBased Graph/Graph/Graph.h"
-#include "../src/PointerBased Graph/AigerReader/AigerReader.h"
-#include "../src/IntegerBased Graph/AigerReaderI/AigerReaderI.h"
-#include "../src/IntegerBased Graph/GraphI/GraphI.h"
+
 
 #include "test_cute.h"
 
 using namespace cute;
 
 int main(int argc, char const *argv[]){
-	/*
-	using namespace std;
-
-	if(argc != 4){
-		cout <<  "*   @param arg[1] Filename without extension" << endl
-			 <<	 "*   @param arg[2] Pointer or Integer based 1 - Pointer 2 - Integer" << endl
-			 <<	 "*   @param arg[3] Bidirection option 1 - Unidirected Graph 2 - Bidirected Graph" << endl;
-		exit(-1);
-	}
-
-	string fileName(arg[1]);
-	int pointerOrInteger = atoi(arg[2]);
-    int bidirectionOption = atoi(arg[3]);
-
-	string filePathAAG = "aags/";
-	string filePathAIG = "aigs/";
-
-	if(pointerOrInteger == 1){  // Pointer Based Graph
-		AigerReader reader(filePathAAG + fileName + ".aag", bidirectionOption);
-	    GRAPH* aig = reader.readAAGFile();
-		reader.generateDot(aig, fileName + ".dot");
-
-		aig->print();
-
-        delete aig;
-	}
-
-	else if(pointerOrInteger == 2){ // Integer Based Graph
-		AigerReaderI reader(filePathAAG + fileName + ".aag", bidirectionOption);
-	    GRAPHI* aig = reader.readAAGFile();
-
-        aig->print();
-
-	}
-
-
-
-	return 0;
-	*/
 
 	using namespace std;
 	suite s;
--- test/test_cute.cpp
+++ test/test_cute.cpp
@@ -8,10 +8,45 @@
 
 #include "test_cute.h"
 
+#include "../src/PointerBased Graph/Graph/AIGNode.h"
+#include "../src/PointerBased Graph/Graph/Graph.h"
+#include "../src/PointerBased Graph/AigerReader/AigerReader.h"
+#include "../src/IntegerBased Graph/AigerReaderI/AigerReaderI.h"
+#include "../src/IntegerBased Graph/GraphI/GraphI.h"
+
 using namespace std;
+
+void test_c17() {
+
+	string fileName = "C17";
+	int pointerOrInteger = 1;
+	int bidirectionOption = 1;
+
+	string filePathAAG = "aags/";
+	string filePathAIG = "aigs/";
+
+	if(pointerOrInteger == 1){  // Pointer Based Graph
+		AigerReader reader(filePathAAG + fileName + ".aag", bidirectionOption);
+		GRAPH* aig = reader.readAAGFile();
+		reader.generateDot(aig, fileName + ".dot");
+
+		aig->print();
+
+		delete aig;
+	}
+
+	else if(pointerOrInteger == 2){ // Integer Based Graph
+		AigerReaderI reader(filePathAAG + fileName + ".aag", bidirectionOption);
+		GRAPHI* aig = reader.readAAGFile();
+
+		aig->print();
+
+	}
+}
+
 
 cute::suite make_suite_test_cute() {
 	cute::suite s;
-
+	s.push_back(CUTE(test_c17));
 	return s;
 }
