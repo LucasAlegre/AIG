d9fc28c587ddacffc36d3036182920cb55056350
marcelo <marcelo@logics-lab>
2017-05-08 15:37:49 -0300

    Add new last parameter 1 - aig, 2 - aag

--- src/main.cpp
+++ src/main.cpp
@@ -32,21 +32,24 @@ using namespace std;
  */
 int main(int argc, char* arg[]){
 
-	if(argc != 4){
+	if(argc != 5){
 		cout <<  "*   @param arg[1] Filename without extension" << endl
 			 <<	 "*   @param arg[2] Pointer or Integer based 1 - Pointer 2 - Integer" << endl
-			 <<	 "*   @param arg[3] Bidirection option 1 - Unidirected Graph 2 - Bidirected Graph" << endl;
+			 <<	 "*   @param arg[3] Bidirection option 1 - Unidirected Graph 2 - Bidirected Graph" << endl
+		     <<	 "*   @param arg[4] AIGER file type 1 - Binary (aig) 2 - ASCII (aag)" << endl;
+
 		exit(-1);
 	}
 
 	string fileName(arg[1]);
 	int pointerOrInteger = atoi(arg[2]);
     int bidirectionOption = atoi(arg[3]);
+    int aigerFileType = atoi(arg[4]);
 
 	string filePathAAG = "aags/";
 	string filePathAIG = "aigs/";
 
-	if(pointerOrInteger == 1){  // Pointer Based Graph
+	if(pointerOrInteger == 1 && aigerFileType == 1 ){  // Pointer Based Graph with aig
 		AigerReader reader(filePathAIG + fileName + ".aig", bidirectionOption);
 	    GRAPH* aig = reader.readAIGFile();
 		reader.generateDot(aig, fileName + ".dot");
@@ -57,7 +60,7 @@ int main(int argc, char* arg[]){
         delete aig;
 	}
 
-	else if(pointerOrInteger == 2){ // Integer Based Graph
+	else if(pointerOrInteger == 2 && aigerFileType == 1){ // Integer Based Graph with aig
 		AigerReaderI reader(filePathAIG + fileName + ".aig", bidirectionOption);
 	    GRAPHI* aig = reader.readAIGFile();
 
@@ -67,6 +70,27 @@ int main(int argc, char* arg[]){
 
 	}
 
+	else if(pointerOrInteger == 1 && aigerFileType == 2 ){  // Pointer Based Graph with aag
+		AigerReader reader(filePathAAG + fileName + ".aag", bidirectionOption);
+	    GRAPH* aig = reader.readAAGFile();
+		reader.generateDot(aig, fileName + ".dot");
+
+		aig->print();
+
+
+        delete aig;
+	}
+
+
+	else if(pointerOrInteger == 2 && aigerFileType == 2){ // Integer Based Graph with aag
+		AigerReaderI reader(filePathAAG + fileName + ".aag", bidirectionOption);
+	    GRAPHI* aig = reader.readAAGFile();
+
+        aig->print();
+
+        delete aig;
+
+	}
 
 
 	return 0;
