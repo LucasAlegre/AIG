9513394c7783390ed41a3ed4582690fcc22713ed
Marcelo Bittencourt <tchelopoa@gmail.com>
2017-06-06 12:53:12 -0300

    Reduce the parameter to only the fileName aag

--- src/main.cpp
+++ src/main.cpp
@@ -27,45 +27,38 @@ using namespace std;
  */
 int main(int argc, char* arg[]){
 
-	if(argc != 5){
-		cout <<  "*   @param arg[1] Filename without extension" << endl
-			 <<	 "*   @param arg[2] Pointer or Integer based 1 - Pointer 2 - Integer" << endl
-			 <<	 "*   @param arg[3] Bidirection option 1 - Unidirected Graph 2 - Bidirected Graph" << endl
-		     <<	 "*   @param arg[4] AIGER file type 1 - Binary (aig) 2 - ASCII (aag)" << endl;
+	string message = "*** AIG Index-based Graph (IG / GI) ***";
+
+	if(argc != 2){
+		cout <<  "*   @param arg[1] Filename without extension" << endl;
+		//			 <<	 "*   @param arg[2] Pointer or Integer based 1 - Pointer 2 - Integer" << endl
+		//			 <<	 "*   @param arg[3] Bidirection option 1 - Unidirected Graph 2 - Bidirected Graph" << endl
+		//		     <<	 "*   @param arg[4] AIGER file type 1 - Binary (aig) 2 - ASCII (aag)" << endl;
 
 		exit(-1);
 	}
 
 	string fileName(arg[1]);
-	int pointerOrInteger = atoi(arg[2]);
-    //int bidirectionOption = atoi(arg[3]);
+	//int pointerOrInteger = atoi(arg[2]);
+	int pointerOrInteger = 2;
+	//int bidirectionOption = atoi(arg[3]);
 	int bidirectionOption = 1;
-    int aigerFileType = atoi(arg[4]);
+	//int aigerFileType = atoi(arg[4]);
+	int aigerFileType = 2;
+
+	cout << message << " FileName: " << fileName << endl;
 
 	string filePathAAG = "aags/";
 	string filePathAIG = "aigs/";
 
+	AigerReaderI reader(filePathAAG + fileName + ".aag", bidirectionOption);
+	GRAPHI* aig = reader.readAAGFile();
 
-	if(pointerOrInteger == 2 && aigerFileType == 1){ // Integer Based Graph with aig
-		AigerReaderI reader(filePathAIG + fileName + ".aig", bidirectionOption);
-	    GRAPHI* aig = reader.readAIGFile();
+	aig->print();
 
-        aig->print();
+	delete aig;
 
-        delete aig;
-
-	}
-
-	else if(pointerOrInteger == 2 && aigerFileType == 2){ // Integer Based Graph with aag
-		AigerReaderI reader(filePathAAG + fileName + ".aag", bidirectionOption);
-	    GRAPHI* aig = reader.readAAGFile();
-
-        aig->print();
-
-        delete aig;
-
-	}
-
+	cout << message << " FileName: " << fileName << endl;
 
 	return 0;
 
