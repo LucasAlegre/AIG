b4b86898a3de9ef56d9d1529e77b62da93cc9f9d
Marcelo Bittencourt <tchelopoa@gmail.com>
2017-06-06 21:26:25 -0300

    Add parameter show Output 0-false 1-true

--- src/main.cpp
+++ src/main.cpp
@@ -27,10 +27,11 @@ using namespace std;
  */
 int main(int argc, char* arg[]){
 
-	string message = "*** AIG Index-based Graph (IG / GI) ***";
+	string message = "********** AIG Index-based Graph (IG / GI) **********";
 
-	if(argc != 2){
-		cout <<  "*   @param arg[1] Filename without extension" << endl;
+	if(argc != 3){
+		cout <<  "*   @param arg[1] Filename without extension" << endl
+		     <<	 "*   @param arg[2] Show output 0 - False 1 - True" << endl;
 		//			 <<	 "*   @param arg[2] Pointer or Integer based 1 - Pointer 2 - Integer" << endl
 		//			 <<	 "*   @param arg[3] Bidirection option 1 - Unidirected Graph 2 - Bidirected Graph" << endl
 		//		     <<	 "*   @param arg[4] AIGER file type 1 - Binary (aig) 2 - ASCII (aag)" << endl;
@@ -39,26 +40,30 @@ int main(int argc, char* arg[]){
 	}
 
 	string fileName(arg[1]);
+	bool showOutput=atoi(arg[2]);
 	//int pointerOrInteger = atoi(arg[2]);
-	int pointerOrInteger = 2;
+    //int pointerOrInteger = 2;
 	//int bidirectionOption = atoi(arg[3]);
 	int bidirectionOption = 1;
 	//int aigerFileType = atoi(arg[4]);
-	int aigerFileType = 2;
+	//int aigerFileType = 2;
 
-	cout << message << " FileName: " << fileName << endl;
+	if (showOutput==true)
+		cout << message << " FileName: " << fileName << endl;
 
 	string filePathAAG = "aags/";
-	string filePathAIG = "aigs/";
+	//string filePathAIG = "aigs/";
 
 	AigerReaderI reader(filePathAAG + fileName + ".aag", bidirectionOption);
 	GRAPHI* aig = reader.readAAGFile();
 
-	aig->print();
+	if (showOutput==true)
+		aig->print();
 
 	delete aig;
 
-	cout << message << " FileName: " << fileName << endl;
+	if (showOutput==true)
+		cout << message << " FileName: " << fileName << endl;
 
 	return 0;
 
